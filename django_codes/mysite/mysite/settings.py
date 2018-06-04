8276b50e30153fd800471619db6f2e4ebfbb652e
Toti Cavalcanti <toticavalcanti@hotmail.com>
2018-06-04 13:45:28 -0300

    update settings.py, the import of Csv from decouple

--- django_codes/mysite/mysite/settings.py
+++ django_codes/mysite/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from decouple import config
+from decouple import config, Csv
 import dj_database_url
 
 # separe a sua SECRET_KEY pois vamos usar ela posteriormente
