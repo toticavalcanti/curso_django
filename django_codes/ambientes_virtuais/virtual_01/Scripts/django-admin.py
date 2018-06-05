998b445d0ce838a4bcbfbb8dd9615f0faab20f1b
Toti Cavalcanti <toticavalcanti@hotmail.com>
2018-06-05 16:29:05 -0300

    delete ambientes_virtuais used in windows system

--- django_codes/ambientes_virtuais/virtual_01/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\toti.cavalcanti\ambientes_virtuais\ambientes_virtuais\virtual_01\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
