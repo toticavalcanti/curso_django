dcbe841330c3430b7e292684d663f40720091302
Toti Cavalcanti <toticavalcanti@hotmail.com>
2018-06-05 16:50:37 -0300

    update settings.py

--- django_codes/mysite/mysite/settings.py
+++ django_codes/mysite/mysite/settings.py
@@ -43,8 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
-    #'polls.apps.PollsConfig',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
