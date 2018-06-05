8ef05cbb172554c500953a42ff7a27309bdb8a27
Toti Cavalcanti <toticavalcanti@hotmail.com>
2018-06-05 16:20:02 -0300

    Upload AmbientePython3

---
+++ django_codes/AmbientePython3/bin/django-admin.py
@@ -0,0 +1,6 @@
+#!/home/toticavalcanti/django_codes/AmbientePython3/bin/python3
+# EASY-INSTALL-DEV-SCRIPT: 'Django==2.1.dev20180410172650','django-admin.py'
+__requires__ = 'Django==2.1.dev20180410172650'
+__import__('pkg_resources').require('Django==2.1.dev20180410172650')
+__file__ = '/home/toticavalcanti/django_codes/django/django/bin/django-admin.py'
+exec(compile(open(__file__).read(), __file__, 'exec'))
