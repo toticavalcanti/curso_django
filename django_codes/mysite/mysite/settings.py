2ea3d0647e4bf89760f99e0c05c0d7ddd2e335d4
Toti Cavalcanti <toticavalcanti@hotmail.com>
2018-07-17 01:46:06 -0300

    update manage.py and settings.py

--- django_codes/mysite/mysite/settings.py
+++ django_codes/mysite/mysite/settings.py
@@ -83,15 +83,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': config('DATABASE_URL'),
-#         'PORT': '',
-#     }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DATABASE_URL'),
+        'PORT': '',
+    }
 #    'default': {
 #       'ENGINE': 'django.db.backends.sqlite3',
 #       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
@@ -105,7 +105,7 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True
     #     'PORT': '5432',
     # }
 
-#}
+}
 
 
 # Password validation
